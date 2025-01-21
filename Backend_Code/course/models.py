d4f04ef857afc79da0b4f5e99664d13425f2c714
kartikprajapti123 <155372801+kartikprajapti123@users.noreply.github.com>
2025-01-21 20:05:00 +0530

    changes

---
+++ Backend_Code/course/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+class Course(models.Model): 
+    course_status_fields=[
+        ("active","active"),
+        ("inactive","inactive")
+        
+    ]
+    course_title = models.CharField(max_length=255, null=True,blank=True)
+    course_teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="course_teacher",blank=True,null=True)
+    course_description = models.TextField(null=True,blank=True)
+    course_status=models.CharField(choices=course_status_fields,default="active")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    deleted=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.course_title
+
+
+class CourseVideo(models.Model):
+    course=models.ForeignKey(Course,on_delete=models.CASCADE,related_name="course_video_course",null=True,blank=True)
+    course_video = models.FileField(upload_to="course_video/",blank=True,null=True)
+    course_video_title = models.CharField(max_length=255, null=True,blank=True)
+    course_video_description = models.TextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    deleted=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.course_video_title
+
+
+class CourseFeedback(models.Model):
+    feedback_student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="feedback_student",blank=True,null=True)
+    feedback_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="feedback_course",blank=True,null=True)
+    feedback_message = models.CharField(max_length=100, null=True,blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    deleted=models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.feedback_student.username} - {self.feedback_message[:20]}"
