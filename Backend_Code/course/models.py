ce1ccc8fd2bd205fd6ad544b31112eb56d81289e
kartikprajapti123 <155372801+kartikprajapti123@users.noreply.github.com>
2025-02-25 23:15:27 +0530

    chnages

--- Backend_Code/course/models.py
+++ Backend_Code/course/models.py
@@ -76,3 +76,26 @@ class CourseFeedback(models.Model):
     
     def __str__(self):
         return f"{self.feedback_student.username} - {self.feedback_message[:20]}"
+
+
+
+
+class CoursePurchase(models.Model):
+    PAYMENT_STATUS = [
+        ("pending", "pending"),
+        ("paid", "paid"),
+        ("failed", "failed"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="orders")
+    razorpay_order_id = models.CharField(max_length=255, blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=255, blank=True, null=True)
+    razorpay_signature = models.CharField(max_length=255, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+    status = models.CharField(choices=PAYMENT_STATUS, default="pending", max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username}"
